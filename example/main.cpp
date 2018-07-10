c142633656006add7488d01059da45034ea0c8de
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2018-07-10 12:20:01 -0300

    Updating the example

--- example/main.cpp
+++ example/main.cpp
@@ -14,6 +14,25 @@ int main()
     else
     {
         cout << "Device disconnected!" << endl;
+
+        return -1;
+    }
+
+    cout << "Reseting the sensor..." << endl;
+    mt9d111.Reset(MT9D111_RESET_SOFT);
+
+    cout << "Loading configuration...";
+    if (mt9d111.Config())
+    {
+        mt9d111.EnablePLL(0x1000, 0x0500);  // ref = 10 MHz, output = 80 MHz
+        mt9d111.SetOutputFormat(MT9D111_OUTPUT_FORMAT_RAW_8);
+        mt9d111.SetResolution(MT9D111_MODE_PREVIEW, 640, 480);
+
+        cout << "SUCCESS!" << endl;
+    }
+    else
+    {
+        cout << "FAILURE!" << endl;
     }
 
     mt9d111.Close();
